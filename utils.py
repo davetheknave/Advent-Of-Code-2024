81e46b21ad98714c6affd4150206d0b9cf396282
David Stearns <knavedavid@gmail.com>
2024-12-01 16:35:18 -0500

    Improve method signature for clarity

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import re
 
-def regex(pattern: str, subject: str):
+def regex(pattern: str, subject: str) -> tuple[str]:
     matches = re.match(pattern, subject)
     return matches.groups() if matches is not None else None
 
