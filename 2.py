ababd5ab942422c764e5c2f3b133501db54cb47a
David Stearns <knavedavid@gmail.com>
2024-12-02 15:40:22 -0500

    Solve day 2 part 2
    Probably could have been more efficient on the second part but it works

--- 2.py
+++ 2.py
@@ -11,36 +11,47 @@ inExample = """7 6 4 2 1
 with open('2.txt', 'r') as f:
     inPuzzle = f.read()
 
+def check_levels(report):
+    increasing = False
+    decreasing = False
+    prev = None
+    for r in report:
+        if prev is not None:
+            change = abs(prev - r)
+            if change > 3 or change < 1:
+                return False
+
+            if r < prev:
+                decreasing = True
+                if increasing:
+                    return False
+
+            elif r > prev:
+                increasing = True
+                if decreasing:
+                    return False
+        prev = r
+
+    return True
+
 def solve(inv):
     # Turn input into a 2D list of ints
     reports = [[int(ll) for ll in l.split()] for l in inv.splitlines()]
     
-    safeCount = 0
+    safeCount = safeCount2 = 0
     for r in reports:
-        safe = True
-        inc = False
-        dec = False
-        prev = None
-        for rr in r:
-            if prev is not None:
-                if rr == prev:
-                    safe = False
-                elif rr < prev:
-                    if inc:
-                        safe = False
-                    dec = True
-                elif rr > prev:
-                    if dec:
-                        safe = False
-                    inc = True
-                change = abs(prev - rr)
-                if change > 3 or change < 1:
-                    safe = False
-            prev = rr
-        if safe:
+        # check safety for first solution
+        if check_levels(r):
             safeCount += 1
+        
+        # check safety for second solution
+        for i,_ in enumerate(r):
+            newList = r[:i] + r[i+1:]  # new list with element at i removed
+            if check_levels(newList):
+                safeCount2 += 1
+                break
     
-    return (safeCount, None)
+    return (safeCount, safeCount2)
 
 sol = solve(inPuzzle)
 print(sol)
