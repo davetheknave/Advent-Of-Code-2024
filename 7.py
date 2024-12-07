193589bed91cfeb35fd89628184a3a9816730602
David Stearns <knavedavid@gmail.com>
2024-12-07 11:29:39 -0500

    Solve day 7 part 2

--- 7.py
+++ 7.py
@@ -6,18 +6,19 @@ from operator import add,mul
 from functools import reduce
 
 firstOperators = [add,mul]
+secondOperators = [add,mul,lambda a,b: int(str(a)+str(b))]
 
-def search(goal, operands, result, operators: list) -> list:
-    if len(operands) <= 0:
+def search(goal, operands, result, operators: list, operatorPool: list) -> list:
+    if len(operands) <= 0 or result > goal:
         if result == goal:
             return operators
         else:
             return None
     foundCorrect = None
-    for fo in firstOperators:
+    for fo in operatorPool:
         nextOperators = operators.copy()
         nextOperators.append(fo)
-        nextSearch = search(goal, operands[1:], fo(result,operands[0]),nextOperators)
+        nextSearch = search(goal, operands[1:], fo(result,operands[0]),nextOperators, operatorPool)
         if nextSearch is not None:
             foundCorrect = nextSearch
     if foundCorrect is not None:
@@ -30,18 +31,25 @@ def solve(inv: str):
     lines = [x.split(':') for x in inPuzzle.splitlines()]
     lines = list(map(lambda a: (int(a[0]),[int(x) for x in a[1].split()]), lines))
 
-    validResults = []
-    results = []
+    validResults1 = []
+    validResults2 = []
+    results1 = []
+    results2 = []
     for l in lines:
-        result = search(l[0], l[1], 0, [])
+        result = search(l[0], l[1], 0, [], firstOperators)
+        result2 = search(l[0], l[1], 0, [], secondOperators)
         if result is not None:
-            validResults.append(l[0])
-        results.append((l[0],result[1:] if result is not None and len(result) > 1 else None))
+            validResults1.append(l[0])
+        if result2 is not None:
+            validResults2.append(l[0])
+        results1.append((l[0],result[1:] if result is not None and len(result) > 1 else None))
+        results2.append((l[0],result2[1:] if result2 is not None and len(result2) > 1 else None))
             
     # for i in results:
     #     print(i)
-    total = reduce(add, validResults) if validResults else 0
-    return (total, None)
+    total = reduce(add, validResults1) if validResults1 else 0
+    total2 = reduce(add, validResults2) if validResults2 else 0
+    return (total, total2)
 
 inExample = """\
 190: 10 19
