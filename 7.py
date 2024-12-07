9435b6b0a5f1430c9bd3f8e42eff08fd80e76c19
David Stearns <knavedavid@gmail.com>
2024-12-07 11:41:56 -0500

    Improve speed and clarity

--- 7.py
+++ 7.py
@@ -5,22 +5,23 @@ import os
 from operator import add,mul
 from functools import reduce
 
-firstOperators = [add,mul]
-secondOperators = [add,mul,lambda a,b: int(str(a)+str(b))]
-
-def search(goal, operands, result, operators: list, operatorPool: list) -> list:
+def findOperation(goal, operands: list, result, operators: list, operatorPool: list) -> list:
+    """Returns a list of operators that can be used to turn the operands int the goal starting from the result, from the operator pool. Returns None if no combination works"""
+    # Base case
     if len(operands) <= 0 or result > goal:
         if result == goal:
             return operators
         else:
             return None
+    # Recursion
     foundCorrect = None
     for fo in operatorPool:
         nextOperators = operators.copy()
         nextOperators.append(fo)
-        nextSearch = search(goal, operands[1:], fo(result,operands[0]),nextOperators, operatorPool)
+        nextSearch = findOperation(goal, operands[1:], fo(result,operands[0]),nextOperators, operatorPool)
         if nextSearch is not None:
             foundCorrect = nextSearch
+            break
     if foundCorrect is not None:
         return foundCorrect
     else:
@@ -33,20 +34,14 @@ def solve(inv: str):
 
     validResults1 = []
     validResults2 = []
-    results1 = []
-    results2 = []
     for l in lines:
-        result = search(l[0], l[1], 0, [], firstOperators)
-        result2 = search(l[0], l[1], 0, [], secondOperators)
+        result = findOperation(l[0], l[1], 0, [], [add,mul])
+        result2 = findOperation(l[0], l[1], 0, [], [add,mul,lambda a,b: int(str(a)+str(b))])
         if result is not None:
             validResults1.append(l[0])
         if result2 is not None:
             validResults2.append(l[0])
-        results1.append((l[0],result[1:] if result is not None and len(result) > 1 else None))
-        results2.append((l[0],result2[1:] if result2 is not None and len(result2) > 1 else None))
             
-    # for i in results:
-    #     print(i)
     total = reduce(add, validResults1) if validResults1 else 0
     total2 = reduce(add, validResults2) if validResults2 else 0
     return (total, total2)
