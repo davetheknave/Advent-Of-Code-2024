c840497577f1bb032c5f667e2e371f0a1cb30abf
David Stearns <knavedavid@gmail.com>
2024-12-11 14:25:37 -0500

    Improve clarity

--- 11.py
+++ 11.py
@@ -5,45 +5,45 @@ import os
 import math
 from collections import defaultdict
 
-def digits(value):
+def digits(value)->int:
     return math.floor(math.log10(value)+1)
-def split(value,splitPoint):
-    first = value//(10**splitPoint)
-    second = value - (first*(10**splitPoint))
-    return [first,second]
-def blink(value):
-    if value == 0:
-        return [1]
-    d = digits(value)
-    if d % 2 == 0:
-        return split(value,d//2)
-    return [value * 2024]
 
-def blink_many(stones,count):
-    for i in range(count):
-        for k,v in list(stones.items()):
-            stones[k] -= v
-            for i in blink(k):
-                stones[i] += v
-        for k in list(stones.keys()):
-            if stones[k] == 0:
-                del stones[k]
+def blink_one(stone: int)->[int]:
+    if stone == 0:
+        return [1]
+    d = digits(stone)
+    if d % 2 == 0:
+        first = stone//(10**(d//2))
+        second = stone - (first*(10**(d//2)))
+        return [first,second]
+    return [stone * 2024]
+
+def blink_all(stones: dict, count=1)->dict:
+    for _ in range(count):
+        for stone,count in list(stones.items()):
+            stones[stone] -= count
+            for newStone in blink_one(stone):
+                stones[newStone] += count
+
+        # This just saves time
+        for stone,count in list(stones.items()):
+            if count == 0:
+                del stones[stone]
     return stones
 
-def count_stones(stones):
+def count_stones(stones: dict)->int:
     count = 0
     for s in stones.values():
         count += s
     return count
 
 def solve(inv: str):
-    inv = [int(x) for x in inv.split()]
     stones = defaultdict(int)
-    for s in inv:
+    for s in [int(x) for x in inv.split()]:
         stones[s] += 1
     
-    blinks1 = blink_many(stones.copy(),25)
-    blinks2 = blink_many(stones.copy(),75)
+    blinks1 = blink_all(stones.copy(),25)
+    blinks2 = blink_all(stones.copy(),75)
     
     return (count_stones(blinks1),count_stones(blinks2))
 
